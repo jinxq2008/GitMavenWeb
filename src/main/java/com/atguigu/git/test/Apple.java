720242db08abb56e9d273de1b2ee72c0bdeb937d
jinxq2008 <147451990@qq.com>
2019-06-09 00:30:31 +0800

    2

---
+++ src/main/java/com/atguigu/git/test/Apple.java
@@ -0,0 +1,15 @@
+package com.atguigu.git.test;
+
+public class Apple {
+	public static void main(String[] args) {
+		
+		System.out.println("edit by xiaoqiang sina ");
+		System.out.println("edit by xiaoqiang sina ");
+		System.out.println("edit by xiaoqiang sina ");
+		System.out.println("edit by xiaoqiang sina ");
+		//System.out.println("edit by xiaoqiang sina ");
+		//6
+		
+	}
+
+}
